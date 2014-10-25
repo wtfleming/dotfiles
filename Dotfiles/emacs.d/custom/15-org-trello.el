19d672d1f05a5d2fb99c1f0b81f1135142614a8a
Will Fleming <wfleming77@gmail.com>
2014-10-24 22:15:15 -0700

    don't use org-trello

--- Dotfiles/emacs.d/custom/15-org-trello.el
+++
@@ -1,4 +0,0 @@
-(require 'org-trello)
-;; to have org-trello activated for each org file, uncomment this
-;; (add-hook 'org-mode-hook 'org-trello-mode)
-;; otherwise, M-x org-trello-mode in an org buffer to activate org-trello
