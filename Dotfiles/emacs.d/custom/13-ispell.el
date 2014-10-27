40ffa3b16e3e6ebb2d991f0c031421f359916cc0
Will Fleming <wfleming77@gmail.com>
2014-10-26 19:28:18 -0700

    add location for ispell from macports

--- Dotfiles/emacs.d/custom/13-ispell.el
+++ Dotfiles/emacs.d/custom/13-ispell.el
@@ -1,3 +1,8 @@
-;; ispell
+;; ispell macports
 (if (system-type-is-darwin)
-  (setq ispell-program-name "/usr/local/bin/ispell"))
+  (setq ispell-program-name "/opt/local/bin/ispell"))
+
+
+;; ispell homebrew
+;(if (system-type-is-darwin)
+;  (setq ispell-program-name "/usr/local/bin/ispell"))
