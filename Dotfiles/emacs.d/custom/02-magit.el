72993095c1ff001f85f213a7937d57bd2cfd2b45
Will Fleming <wfleming77@gmail.com>
2014-11-26 16:01:08 -0800

    Make homebrew emacs work

--- Dotfiles/emacs.d/custom/02-magit.el
+++ Dotfiles/emacs.d/custom/02-magit.el
@@ -1,11 +1,10 @@
 ;; magit
 (global-set-key (kbd "C-c m") 'magit-status)
 
-;; On OS X make magit commits work with emacs installed via macports
-(if (system-type-is-darwin)
-  (set-variable 'magit-emacsclient-executable "/Applications/MacPorts/Emacs.app/Contents/MacOS/bin/emacsclient"))
-
-
 ;; On OS X make magit commits work with emacs installed via homebrew
+(if (system-type-is-darwin)
+  (set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.4/bin/emacsclient"))
+
+;; On OS X make magit commits work with emacs installed via macports
 ;(if (system-type-is-darwin)
-;  (set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient"))
+;  (set-variable 'magit-emacsclient-executable "/Applications/MacPorts/Emacs.app/Contents/MacOS/bin/emacsclient"))
