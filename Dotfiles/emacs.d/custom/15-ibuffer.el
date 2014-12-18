6f4b3c5a3abe026982e404b58b42b3f6626ce540
Will Fleming <wfleming77@gmail.com>
2014-12-17 22:55:49 -0800

    Config ibuffer

--- Dotfiles/emacs.d/custom/15-ibuffer.el
+++ Dotfiles/emacs.d/custom/15-ibuffer.el
@@ -1,15 +1,27 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 
+;; Show org-agenda files in own group
+;; http://emacs.stackexchange.com/questions/2087/predicates-in-ibuffer-saved-filter-groups
+(defun my-org-agenda-filter ()
+  (let ((fname (buffer-file-name)))
+    (and fname
+         (member (file-truename fname)
+                 (mapcar 'file-truename (org-agenda-files))))))
+
+;; Show groups
 (setq ibuffer-saved-filter-groups
       (quote (("default"
+               ("org-agenda" (or
+                              (mode . org-agenda-mode)
+                              (predicate . (my-org-agenda-filter))))
                ("dired" (mode . dired-mode))
                ("erc" (mode . erc-mode))
                ("emacs" (or
                          (name . "^\\*scratch\\*$")
                          (name . "^\\*Completions\\*$")
                          (name . "^\\*Messages\\*$")))
-               ("org" (mode . org-mode))
+               ("org-mode" (mode . org-mode))
                ("Programming"
                 (or
                  (mode . pig-mode)
@@ -25,6 +37,10 @@
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "default")))
 
+
+
+
+;; ---------- display --------------------------------
 ;; Display human readable buffer sizes
 (define-ibuffer-column size-h
   (:name "Size")
@@ -42,3 +58,17 @@
               (size-h 9 -1 :right)
               " "
               filename-and-process)))
+
+;; --------------------------------------
+
+
+;; Switching to ibuffer puts the cursor on the most recent buffer
+  (defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+    "Open ibuffer with cursor pointed to most recent buffer name"
+    (let ((recent-buffer-name (buffer-name)))
+      ad-do-it
+      (ibuffer-jump-to-buffer recent-buffer-name)))
+  (ad-activate 'ibuffer)
+
+;; Hide the summary at the bottom of the buffer
+(setq ibuffer-display-summary nil)
