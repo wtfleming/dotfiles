67fdf148cbc07dcabd0b27fba1f2a98a68779652
Will Fleming <wfleming77@gmail.com>
2014-12-19 19:01:26 -0800

    Add cider-nrepl dependency and update versions

--- Dotfiles/lein/profiles.clj
+++ Dotfiles/lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[jonase/eastwood "0.1.0"]
+{:user {:plugins [[jonase/eastwood "0.2.0"]
                   [lein-cljsbuild "1.0.3"]
-                  [lein-try "0.4.1"]
+                  [lein-try "0.4.3"]
+                  [cider/cider-nrepl "0.8.2-SNAPSHOT"]
                   [codox "0.8.10"]] }}
