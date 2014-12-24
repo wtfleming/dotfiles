a7cb6d58fc151c64431c5ded294d08f37981cf40
Will Fleming <wfleming77@gmail.com>
2014-12-23 18:36:04 -0800

    Add lein-simpleton plugin

--- Dotfiles/lein/profiles.clj
+++ Dotfiles/lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[jonase/eastwood "0.2.0"]
                   [lein-cljsbuild "1.0.3"]
                   [lein-try "0.4.3"]
+                  [lein-simpleton "1.3.0"]
                   [cider/cider-nrepl "0.8.2-SNAPSHOT"]
                   [codox "0.8.10"]] }}
