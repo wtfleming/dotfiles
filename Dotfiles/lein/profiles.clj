41b52b4f3e47702dcb487eae91c718e5573d72fc
Will Fleming <wfleming77@gmail.com>
2015-01-11 21:01:42 -0800

    Update lein-cljsbuild to 1.0.4

--- Dotfiles/lein/profiles.clj
+++ Dotfiles/lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[jonase/eastwood "0.2.0"]
-                  [lein-cljsbuild "1.0.3"]
+                  [lein-cljsbuild "1.0.4"]
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.8.2-SNAPSHOT"]
                   [codox "0.8.10"]] }}
