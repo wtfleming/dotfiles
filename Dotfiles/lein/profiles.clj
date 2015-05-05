42563e53b68b496c56a5032e176270d799e5a036
Will Fleming <will@ubermedia.com>
2015-05-04 19:53:11 -0700

    Add dependency on lein-test-refresh plugin

--- Dotfiles/lein/profiles.clj
+++ Dotfiles/lein/profiles.clj
@@ -2,6 +2,7 @@
                   [lein-cljsbuild "1.0.4"]
                   [lein-try "0.4.3"]
                   [lein-simpleton "1.3.0"]
+                  [com.jakemccrary/lein-test-refresh "0.9.0"]
                   [lein-ancient "0.6.6"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [codox "0.8.10"]] }}
