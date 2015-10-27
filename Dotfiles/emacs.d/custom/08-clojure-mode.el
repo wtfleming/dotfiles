ae35dfaf95555d10f8578dd0caf620dac9f91314
Will Fleming <wfleming77@gmail.com>
2015-10-26 22:29:15 -0700

    Ensure clojure-mode is installed from melpa stable

--- Dotfiles/emacs.d/custom/08-clojure-mode.el
+++ Dotfiles/emacs.d/custom/08-clojure-mode.el
@@ -1,3 +1,5 @@
 ;; Ensure we only indent 2 spaces for clojure-mode
 ;(setq lisp-indent-offset 2)
 ;(setq clojure-defun-style-default-indent t)
+
+(eval-after-load 'clojure-mode '(require 'clojure-mode-extra-font-locking))
