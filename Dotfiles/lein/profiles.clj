58aadfe79b29cdc0918b6438b0710da9d73f6639
Will Fleming <wfleming77@gmail.com>
2015-11-18 13:12:35 -0800

    disable lein plugins for now

--- Dotfiles/lein/profiles.clj
+++ Dotfiles/lein/profiles.clj
@@ -1,10 +1,12 @@
-{:user {:plugins [[jonase/eastwood "0.2.1"]
-                  [lein-cljsbuild "1.1.0"]
-                  [lein-try "0.4.3"]
-                  [lein-simpleton "1.3.0"]
-                  [com.jakemccrary/lein-test-refresh "0.11.0"]
-                  [lein-kibit "0.1.2"]
-                  [lein-pprint "1.1.2"]
-                  [lein-ancient "0.6.7"]
-                  [cider/cider-nrepl "0.9.1"]
-                  [codox "0.8.15"]] }}
+{:user {:plugins [
+                  ;; [jonase/eastwood "0.2.1"]
+                  ;; [lein-cljsbuild "1.1.1"]
+                  ;; [lein-try "0.4.3"]
+                  ;; [lein-simpleton "1.3.0"]
+                  ;; [com.jakemccrary/lein-test-refresh "0.11.0"]
+                  ;; [lein-kibit "0.1.2"]
+                  ;; [lein-pprint "1.1.2"]
+                  ;; [lein-ancient "0.6.7"]
+                  ;; [cider/cider-nrepl "0.9.1"]
+                  ;; [codox "0.8.15"]
+                  ] }}
