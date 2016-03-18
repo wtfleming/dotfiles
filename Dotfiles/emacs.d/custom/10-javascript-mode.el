b52c2fb795099c06034587ff67c66de02dcd1d0b
Will Fleming <wfleming77@gmail.com>
2016-03-17 19:03:26 -0700

    javascript mode config

--- Dotfiles/emacs.d/custom/10-javascript-mode.el
+++ Dotfiles/emacs.d/custom/10-javascript-mode.el
@@ -1,2 +1,10 @@
 ;; Javascript mode
-(setq js-indent-level 2)
+;(setq js-indent-level 2)
+
+(add-hook 'js-mode-hook 'js2-minor-mode)
+
+; Use tabs instead of spaces just for javascript mode
+(add-hook 'js-mode-hook (lambda ()
+                          (setq c-basic-offset 4
+                                tab-width 4
+                                indent-tabs-mode t)))
