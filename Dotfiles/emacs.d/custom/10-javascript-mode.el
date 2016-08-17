4c9f34f7ca7348693c7279d291576e6e90473700
Will Fleming <wfleming77@gmail.com>
2016-08-17 12:52:07 -0700

    use js2-mode and tern

--- Dotfiles/emacs.d/custom/10-javascript-mode.el
+++ Dotfiles/emacs.d/custom/10-javascript-mode.el
@@ -1,10 +1,32 @@
 ;; Javascript mode
-;(setq js-indent-level 2)
 
-(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-; Use tabs instead of spaces just for javascript mode
-(add-hook 'js-mode-hook (lambda ()
+;; ; Use tabs instead of spaces just for javascript mode
+(add-hook 'js2-mode-hook (lambda ()
                           (setq c-basic-offset 4
                                 tab-width 4
                                 indent-tabs-mode t)))
+
+
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+  ;; (setq tern-command (cons (executable-find "tern") '()))
+  ;; (eval-after-load 'tern
+  ;;   '(progn
+  ;;      (require 'tern-auto-complete)
+  ;;      (tern-ac-setup)))
+
+
+
+;; Old js-mode config:
+
+;; (setq js-indent-level 2)
+
+;; (add-hook 'js-mode-hook 'js2-minor-mode)
+
+;; ; Use tabs instead of spaces just for javascript mode
+;; (add-hook 'js-mode-hook (lambda ()
+;;                           (setq c-basic-offset 4
+;;                                 tab-width 4
+;;                                 indent-tabs-mode t)))
