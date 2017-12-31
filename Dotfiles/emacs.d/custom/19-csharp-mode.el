21faaa311f85260569a22cd1a9060cfde96d7be8
Will Fleming <wfleming77@gmail.com>
2017-12-31 16:18:03 -0500

    Configure csharp-mode

--- Dotfiles/emacs.d/custom/19-csharp-mode.el
+++ Dotfiles/emacs.d/custom/19-csharp-mode.el
@@ -1,3 +1,12 @@
+(defun my-csharp-mode-setup ()
+  (setq indent-tabs-mode nil)
+  (setq c-syntactic-indentation t)
+  (setq c-basic-offset 2)
+  (setq tab-width 2)
+  (local-set-key (kbd "C-c C-c") 'recompile))
+
+(add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
+
 (add-hook 'csharp-mode-hook 'flycheck-mode)
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
 
