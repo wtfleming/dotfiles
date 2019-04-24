9e6bd1676c71ded7c544992a3dffb787eeefed78
Will Fleming <wfleming77@gmail.com>
2019-04-24 17:12:02 -0400

    Globally ignore node_modules in projectile

--- Dotfiles/emacs.d/custom/01-common-setup.el
+++ Dotfiles/emacs.d/custom/01-common-setup.el
@@ -47,7 +47,7 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
 
 ;; Enable semantic-mode
 (semantic-mode 1)
