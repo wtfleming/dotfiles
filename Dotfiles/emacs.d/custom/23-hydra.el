896888c1ce1fa357d90dde46cca51426f79d117b
Will Fleming <wfleming77@gmail.com>
2019-05-15 17:59:47 -0400

    add multiple-cursors hydra

--- Dotfiles/emacs.d/custom/23-hydra.el
+++ Dotfiles/emacs.d/custom/23-hydra.el
@@ -349,3 +349,29 @@ _h_   _l_   _o_k        _y_ank
    ("o" (my/insert-unicode "DEGREE SIGN"))
    ("a" (my/insert-unicode "RIGHTWARDS ARROW"))
    ("m" (my/insert-unicode "MICRO SIGN"))))
+
+;; TODO bind this to a key
+(defhydra hydra-multiple-cursors (:hint nil)
+  "
+ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search
+ [Click] Cursor at point       [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil))
