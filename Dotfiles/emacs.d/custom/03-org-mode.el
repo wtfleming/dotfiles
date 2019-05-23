8ed1b0479e6a92a2477cc72486cd115a1aa7e824
Will Fleming <wfleming77@gmail.com>
2019-05-23 15:13:28 -0400

    Use unicode symbol to display org-mode checkboxes

--- Dotfiles/emacs.d/custom/03-org-mode.el
+++ Dotfiles/emacs.d/custom/03-org-mode.el
@@ -40,3 +40,22 @@
 (setq org-export-html-style
       (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" (expand-file-name org-directory) "css/solarized-light.min.css\" />"))
 
+
+;; Use unicode symbol to display org-mode checkboxes
+;; https://blog.jft.rocks/emacs/unicode-for-orgmode-checkboxes.html
+(add-hook 'org-mode-hook (lambda ()
+  "Beautify Org Checkbox Symbol"
+  (push '("[ ]" . "☐") prettify-symbols-alist)
+  (push '("[X]" . "☑") prettify-symbols-alist)
+  (push '("[-]" . "❍") prettify-symbols-alist)
+  (prettify-symbols-mode)))
+
+(defface org-checkbox-done-text
+  '((t (:foreground "#71696A" :strike-through t)))
+  "Face for the text part of a checked org-mode checkbox.")
+
+(font-lock-add-keywords
+ 'org-mode
+ `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
+    1 'org-checkbox-done-text prepend))
+ 'append)
