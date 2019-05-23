d8a4cdbd10dc915a74073c7d98c435d2eea1f08c
Will Fleming <wfleming77@gmail.com>
2019-05-23 19:20:15 -0400

    update js2-mode config

--- Dotfiles/emacs.d/custom/10-javascript-mode.el
+++ Dotfiles/emacs.d/custom/10-javascript-mode.el
@@ -3,15 +3,12 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 ; (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
-;; ; Use tabs instead of spaces just for javascript mode
-;; (add-hook 'js2-mode-hook (lambda ()
-;;                           (setq c-basic-offset 4
-;;                                 tab-width 4
-;;                                 indent-tabs-mode t)))
 
 (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (add-hook 'js2-mode-hook (lambda () (subword-mode +1)))
+;;(add-hook 'js2-mode-hook (lambda () (flycheck-mode t)))
+
 
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-tern))
@@ -21,15 +18,3 @@
 ;;             (flycheck-mode t)
 ;;             (when (executable-find "eslint")
 ;;               (flycheck-select-checker 'javascript-eslint))))
-
-;; Old js-mode config:
-
-;; (setq js-indent-level 2)
-
-;; (add-hook 'js-mode-hook 'js2-minor-mode)
-
-;; ; Use tabs instead of spaces just for javascript mode
-;; (add-hook 'js-mode-hook (lambda ()
-;;                           (setq c-basic-offset 4
-;;                                 tab-width 4
-;;                                 indent-tabs-mode t)))
