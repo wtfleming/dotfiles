79e8b0070b0ecd16d08dcbb9b5812d7e34e36081
Will Fleming <wfleming77@gmail.com>
2019-05-28 18:03:26 -0400

    use js2-mode when editing json

--- Dotfiles/emacs.d/custom/10-javascript-mode.el
+++ Dotfiles/emacs.d/custom/10-javascript-mode.el
@@ -1,6 +1,7 @@
 ;; Javascript mode
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
 ; (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
 
