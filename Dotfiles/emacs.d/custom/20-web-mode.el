0c14477f701b2692c46fb2a49a6c98f090571aaf
Will Fleming <wfleming77@gmail.com>
2019-05-28 17:50:13 -0400

    update web-mode config

--- Dotfiles/emacs.d/custom/20-web-mode.el
+++ Dotfiles/emacs.d/custom/20-web-mode.el
@@ -7,6 +7,17 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
-; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
+
+(setq web-mode-markup-indent-offset 2
+      web-mode-css-indent-offset 2
+      web-mode-code-indent-offset 2
+      web-mode-block-padding 2
+      web-mode-comment-style 2
+
+      web-mode-enable-css-colorization t
+      web-mode-enable-auto-pairing t
+      web-mode-enable-comment-keywords t
+      web-mode-enable-current-element-highlight t)
