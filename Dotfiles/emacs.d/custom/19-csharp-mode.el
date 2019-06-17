71f7537fc23d61d25ab64105bef8673abc5faab1
Will Fleming <wfleming77@gmail.com>
2019-06-17 09:55:49 -0400

    update omnisharp config

--- Dotfiles/emacs.d/custom/19-csharp-mode.el
+++ Dotfiles/emacs.d/custom/19-csharp-mode.el
@@ -1,9 +1,16 @@
 (defun my-csharp-mode-setup ()
   (setq indent-tabs-mode nil)
   (setq c-syntactic-indentation t)
+  (c-set-style "k&r")
   (setq c-basic-offset 2)
+  (setq truncate-lines t)
   (setq tab-width 2)
+  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
   (local-set-key (kbd "C-c C-c") 'recompile))
+
+;csharp-mode README.md recommends this too
+;(electric-pair-mode 1)       ;; Emacs 24
+;(electric-pair-local-mode 1) ;; Emacs 25
 
 (add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
 
