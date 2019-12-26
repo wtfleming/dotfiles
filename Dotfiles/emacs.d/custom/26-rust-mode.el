e2334be4a26b97e3dfa17e6d37ea8a07def86e32
Will Fleming <wfleming77@gmail.com>
2019-12-26 11:16:34 -0500

    add rust-mode to emacs config

---
+++ Dotfiles/emacs.d/custom/26-rust-mode.el
@@ -0,0 +1,9 @@
+(require 'rust-mode)
+
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+
+;; (setq rust-format-on-save t)
+
+(with-eval-after-load 'rust-mode
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
