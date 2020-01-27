d1cee9edd844de0b212c5bb970d484074f305260
Will Fleming <wfleming77@gmail.com>
2020-01-27 10:54:30 -0500

    add lsp-mode and set up rust-mode to use it

--- Dotfiles/emacs.d/custom/26-rust-mode.el
+++ Dotfiles/emacs.d/custom/26-rust-mode.el
@@ -11,3 +11,6 @@
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 (add-hook 'rust-mode-hook (lambda () (flycheck-mode +1)))
+
+;; Run this command in a terminal to install the rust language server
+;; rustup component add rls rust-analysis rust-src
