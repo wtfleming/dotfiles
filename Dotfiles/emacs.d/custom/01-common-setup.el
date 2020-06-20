1dfe79d6adacd508e4e227b0c8b0d7949b2259a4
Will Fleming <wfleming77@gmail.com>
2020-06-20 16:41:51 -0400

    use-package for web-mode

--- Dotfiles/emacs.d/custom/01-common-setup.el
+++ Dotfiles/emacs.d/custom/01-common-setup.el
@@ -24,7 +24,7 @@
    (or (package-installed-p package)
        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
            (package-install package))))
- '(all-the-icons beacon cargo color-identifiers-mode company company-lsp csharp-mode doom-modeline elixir-mode exunit flycheck-inline flycheck-rust git-gutter helm helm-lsp helm-projectile hydra js2-mode lsp-mode lsp-ui lua-mode multiple-cursors neotree omnisharp projectile rainbow-delimiters rainbow-mode restclient rust-mode scala-mode shader-mode tern tide web-mode yasnippet use-package))
+ '(all-the-icons beacon cargo color-identifiers-mode company company-lsp csharp-mode doom-modeline elixir-mode exunit flycheck-inline flycheck-rust git-gutter helm helm-lsp helm-projectile hydra js2-mode lsp-mode lsp-ui lua-mode multiple-cursors neotree omnisharp projectile rainbow-delimiters rainbow-mode restclient rust-mode scala-mode shader-mode tern tide yasnippet use-package))
 
 
 ;; Note that for all-the-icons to work you must manually install them by calling
--- Dotfiles/emacs.d/custom/20-web-mode.el
+++ Dotfiles/emacs.d/custom/20-web-mode.el
@@ -1,25 +1,27 @@
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(use-package web-mode
+  :ensure t
+  :mode
+  (("\\.phtml\\'" . web-mode)
+  ("\\.tpl\\.php\\'" . web-mode)
+  ("\\.jsp\\'" . web-mode)
+  ("\\.as[cp]x\\'" . web-mode)
+  ("\\.erb\\'" . web-mode)
+  ("\\.mustache\\'" . web-mode)
+  ("\\.djhtml\\'" . web-mode)
+  ("\\.jst.ejs\\'" . web-mode)
+  ("\\.html?\\'" . web-mode)
+  ("\\.jsx$" . web-mode)
+  ("\\.tsx$" . web-mode)
+  ("\\.eex\\'" . web-mode)
+  ("\\.leex\\'" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-block-padding 2
+        web-mode-comment-style 2
 
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.leex\\'" . web-mode))
-
-(setq web-mode-markup-indent-offset 2
-      web-mode-css-indent-offset 2
-      web-mode-code-indent-offset 2
-      web-mode-block-padding 2
-      web-mode-comment-style 2
-
-      web-mode-enable-css-colorization t
-      web-mode-enable-auto-pairing t
-      web-mode-enable-comment-keywords t
-      web-mode-enable-current-element-highlight t)
+        web-mode-enable-css-colorization t
+        web-mode-enable-auto-pairing t
+        web-mode-enable-comment-keywords t
+        web-mode-enable-current-element-highlight t))
