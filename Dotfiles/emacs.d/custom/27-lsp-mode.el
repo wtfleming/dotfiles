b152dbfbd1c34448926f784ef8a0ea1e3ab68414
Will Fleming <wfleming77@gmail.com>
2021-01-13 17:39:32 -0500

    config lsp-mode to work better with go

--- Dotfiles/emacs.d/custom/27-lsp-mode.el
+++ Dotfiles/emacs.d/custom/27-lsp-mode.el
@@ -1,18 +1,40 @@
-;; ---- lsp-mode ----
-(require 'lsp-mode)
-(setq lsp-enable-snippet t)
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred)
+        (rust-mode . lsp-deferred)
+        (scala-mode . lsp-deferred))
 
-;; ---- lsp-ui ----
-(require 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
-(setq lsp-ui-doc-enable t)
-(setq lsp-ui-doc-position 'bottom)
-(setq lsp-ui-doc-delay 1.0)
+;; (setq lsp-enable-snippet t)
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :init
+)
+
+(setq lsp-ui-doc-enable nil
+      lsp-ui-doc-position 'bottom
+      lsp-ui-doc-delay 1.0
+      lsp-ui-peek-enable t
+      lsp-ui-sideline-enable t
+      lsp-ui-imenu-enable t
+      lsp-ui-flycheck-enable t)
 
 ;; ---- Rust ----
 ;; Run this command in a terminal to install the rust language server:
 ;; rustup component add rls rust-analysis rust-src
-(add-hook 'rust-mode-hook #'lsp-deferred)
+
+
+;;  ---- Go----
+;; go get golang.org/x/tools/gopls
+;; GO111MODULE=on go get golang.org/x/tools/gopls@latest
+;;(add-hook 'go-mode-hook #'lsp-deferred)
+(setq lsp-gopls-staticcheck t)
+(setq lsp-eldoc-render-all t)
+(setq lsp-gopls-complete-unimported t)
+
+
 
 ;; ---- Elixir ----
 ;; Clone https://github.com/elixir-lsp/elixir-ls
@@ -25,30 +47,25 @@
 ;; Then run asdf global for elixir and erlang
 ;; elixir 1.10.3-otp-23
 ;; erlang 23.0
-(add-to-list 'exec-path "~/bin/elixir-ls")
-(add-hook 'elixir-mode-hook #'lsp-deferred)
+;; (add-to-list 'exec-path "~/bin/elixir-ls")
+;; (add-hook 'elixir-mode-hook #'lsp-deferred)
 
-;; Ignore these directories in elixir projects
-(push "[/\\\\]\\deps$" lsp-file-watch-ignored)
-(push "[/\\\\]\\.elixir_ls$" lsp-file-watch-ignored)
-(push "[/\\\\]_build$" lsp-file-watch-ignored)
+;; ;; Ignore these directories in elixir projects
+;; (push "[/\\\\]\\deps$" lsp-file-watch-ignored)
+;; (push "[/\\\\]\\.elixir_ls$" lsp-file-watch-ignored)
+;; (push "[/\\\\]_build$" lsp-file-watch-ignored)
 
-(defvar lsp-elixir--config-options (make-hash-table))
-(add-hook 'lsp-after-initialize-hook
-          (lambda ()
-            (lsp--set-configuration `(:elixirLS, lsp-elixir--config-options))))
+;; (defvar lsp-elixir--config-options (make-hash-table))
+;; (add-hook 'lsp-after-initialize-hook
+;;           (lambda ()
+;;             (lsp--set-configuration `(:elixirLS, lsp-elixir--config-options))))
 
 
 ;; ---- Scala ----
-(add-hook 'scala-mode-hook #'lsp-deferred)
+;; (add-hook 'scala-mode-hook #'lsp-deferred)
 
-;; (use-package lsp-mode
-;;   ;; Optional - enable lsp-mode automatically in scala files
-;;   :hook  (scala-mode . lsp)
-;;          (lsp-mode . lsp-lens-mode)
-;;   :config (setq lsp-prefer-flymake nil))
 
-;; C++
+;; ---- C++ ----
 ;; To install the language server
 ;; $ brew install ccls
 ;; (use-package ccls
@@ -57,12 +74,3 @@
 ;;          (lambda () (require 'ccls) (lsp))))
 
 ;; (add-hook 'c++-mode-hook #'lsp-deferred)
-
-
-;;  ---- Go----
-;; go get golang.org/x/tools/gopls
-;; GO111MODULE=on go get golang.org/x/tools/gopls@latest
-(add-hook 'go-mode-hook #'lsp-deferred)
-(setq lsp-gopls-staticcheck t)
-(setq lsp-eldoc-render-all t)
-(setq lsp-gopls-complete-unimported t)
