fc0b83337c7699cc62fb18023495bcc659c488d6
Will Fleming <wfleming77@gmail.com>
2021-01-13 20:11:28 -0500

    move javascript config

--- Dotfiles/emacs.d/custom/28-programming.el
+++ Dotfiles/emacs.d/custom/28-programming.el
@@ -57,6 +57,29 @@
 
 (setq typescript-indent-level 2)
 
+;; ------- JavaScript -------
+;; Javascript mode
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+; (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
+
+
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(add-hook 'js2-mode-hook (lambda () (subword-mode +1)))
+;;(add-hook 'js2-mode-hook (lambda () (flycheck-mode t)))
+
+
+;; (eval-after-load 'company
+;;   '(add-to-list 'company-backends 'company-tern))
+
+;; (add-hook 'js2-mode-hook
+;;           (defun my-js2-mode-setup ()
+;;             (flycheck-mode t)
+;;             (when (executable-find "eslint")
+;;               (flycheck-select-checker 'javascript-eslint))))
+
 ;; ------- C# -------
 (defun my-csharp-mode-setup ()
   (setq indent-tabs-mode nil)
