07e1f508f1a69becc9152c16caccc604d25cdcf0
Will Fleming <wfleming77@gmail.com>
2021-01-14 19:28:57 -0500

    refactor helm config

--- Dotfiles/emacs.d/custom/16-helm.el
+++ Dotfiles/emacs.d/custom/16-helm.el
@@ -1,4 +1,38 @@
-(require 'helm-config)
+(use-package helm
+  :ensure t
+  :bind (("C-x b" . helm-mini)
+         ("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x C-f" . helm-find-files)
+         ("C-c C-h SPC" . helm-all-mark-rings))
+  :config
+  (progn
+    (require 'helm-config)
+    (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+          helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+          helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+          helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+          helm-ff-file-name-history-use-recentf t
+          helm-M-x-fuzzy-match                  t
+          helm-buffers-fuzzy-matching           t
+          helm-recentf-fuzzy-match              t)
+    (helm-mode 1)))
+
+;; Currently bound to a hydra
+;;(global-set-key (kbd "C-c p h") 'helm-projectile)
+
+;;(global-set-key (kbd "C-c h o") 'helm-occur)
+;;(global-set-key (kbd "C-c h x") 'helm-register)
+;;(global-set-key (kbd "C-c h g") 'helm-google-suggest)
+;;(global-set-key (kbd "C-c p h") 'helm-projectile)
+
+
+
+(use-package helm-lsp
+  :ensure t)
+
+(use-package helm-projectile
+  :ensure t)
 
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
@@ -6,28 +40,8 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-;;(global-set-key (kbd "C-c h o") 'helm-occur)
-(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
-;;(global-set-key (kbd "C-c h x") 'helm-register)
-;;(global-set-key (kbd "C-c h g") 'helm-google-suggest)
-(global-set-key (kbd "C-c p h") 'helm-projectile)
-
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t)
-
-(setq helm-M-x-fuzzy-match t)
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
 
 ;; Allow arrow keys to once again change directories in helm-find-files
 ;; See https://github.com/emacs-helm/helm/wiki/FAQ#arrow-keys-behavior-have-changed
 (customize-set-variable 'helm-ff-lynx-style-map t)
 
-(helm-mode 1)
