e3d0784289d4b4f7885a2a9490b00e4f5fb0c64c
Will Fleming <wfleming77@gmail.com>
2021-01-15 17:08:05 -0500

    disable omnisharp mode

--- Dotfiles/emacs.d/custom/28-programming.el
+++ Dotfiles/emacs.d/custom/28-programming.el
@@ -111,33 +111,33 @@
 ;;               (flycheck-select-checker 'javascript-eslint))))
 
 ;; ------- C# -------
-(defun my-csharp-mode-setup ()
-  (setq indent-tabs-mode nil)
-  (setq c-syntactic-indentation t)
-  (c-set-style "k&r")
-  (setq c-basic-offset 2)
-  (setq truncate-lines t)
-  (setq tab-width 2)
-  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
-  (local-set-key (kbd "C-c C-c") 'recompile))
+;; (defun my-csharp-mode-setup ()
+;;   (setq indent-tabs-mode nil)
+;;   (setq c-syntactic-indentation t)
+;;   (c-set-style "k&r")
+;;   (setq c-basic-offset 2)
+;;   (setq truncate-lines t)
+;;   (setq tab-width 2)
+;;   (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
+;;   (local-set-key (kbd "C-c C-c") 'recompile))
 
-;; csharp-mode README.md recommends this too
-;; (electric-pair-mode 1)       ;; Emacs 24
-;; (electric-pair-local-mode 1) ;; Emacs 25
+;; ;; csharp-mode README.md recommends this too
+;; ;; (electric-pair-mode 1)       ;; Emacs 24
+;; ;; (electric-pair-local-mode 1) ;; Emacs 25
 
-(add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
+;; (add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
 
-(add-hook 'csharp-mode-hook 'flycheck-mode)
-(add-hook 'csharp-mode-hook 'omnisharp-mode)
+;; (add-hook 'csharp-mode-hook 'flycheck-mode)
+;; (add-hook 'csharp-mode-hook 'omnisharp-mode)
 
-;; Move cursor by camelCase
-(add-hook 'csharp-mode-hook (lambda () (subword-mode +1)))
+;; ;; Move cursor by camelCase
+;; (add-hook 'csharp-mode-hook (lambda () (subword-mode +1)))
 
 
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-omnisharp))
+;; (eval-after-load 'company
+;;   '(add-to-list 'company-backends 'company-omnisharp))
 
-(add-hook 'csharp-mode-hook #'company-mode)
+;; (add-hook 'csharp-mode-hook #'company-mode)
 
 ;; ------- Go -------
 (use-package go-mode
