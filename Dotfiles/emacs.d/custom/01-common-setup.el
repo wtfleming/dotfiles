1081cf2a1085aa1a10a391aa5b1785c66a869e78
Will Fleming <wfleming77@gmail.com>
2021-01-15 17:08:05 -0500

    disable omnisharp mode

--- Dotfiles/emacs.d/custom/01-common-setup.el
+++ Dotfiles/emacs.d/custom/01-common-setup.el
@@ -25,7 +25,7 @@
    (or (package-installed-p package)
        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
            (package-install package))))
- '(all-the-icons beacon color-identifiers-mode csharp-mode doom-modeline elixir-mode exunit flycheck-inline git-gutter hydra js2-mode lua-mode neotree omnisharp projectile rainbow-delimiters rainbow-mode scala-mode shader-mode tern tide lsp-metals))
+ '(all-the-icons beacon color-identifiers-mode csharp-mode doom-modeline elixir-mode exunit flycheck-inline git-gutter hydra js2-mode lua-mode neotree projectile rainbow-delimiters rainbow-mode scala-mode shader-mode tern tide lsp-metals))
 
 
 ;; Note that for all-the-icons to work you must manually install them by calling
