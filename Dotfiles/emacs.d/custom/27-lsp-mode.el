109276d76858e79e650b2ac192bb176874570317
Will Fleming <wfleming77@gmail.com>
2021-02-01 17:10:44 -0500

    Set lsp-mode variables to improve performance

--- Dotfiles/emacs.d/custom/27-lsp-mode.el
+++ Dotfiles/emacs.d/custom/27-lsp-mode.el
@@ -22,6 +22,16 @@
       lsp-ui-imenu-enable t
       lsp-ui-flycheck-enable t)
 
+;; ---- Performance ----
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+
+;; An alternative: follow the method recommended by Gnu Emacs Maintainer Eli Zaretskii: "My suggestion is to repeatedly multiply gc-cons-threshold by 2 until you stop seeing significant improvements in responsiveness, and in any case not to increase by a factor larger than 100 or somesuch. If even a 100-fold increase doesn't help, there's some deeper problem with the Lisp code which produces so much garbage, or maybe GC is not the reason for slowdown."
+(setq gc-cons-threshold 100000000) ;; 100mb like most of the popular starter kits like Spacemacs/Doom/Prelude, etc do
+
+;; Increase the amount of data which Emacs reads from the process. Again the emacs default is too low 4k considering that the some of the language server responses are in 800k - 3M range.
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+
 ;; ---- Rust ----
 ;; Run this command in a terminal to install the rust language server:
 ;; rustup component add rls rust-analysis rust-src
