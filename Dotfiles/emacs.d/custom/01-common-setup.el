5ead031cfeba770ca7f0837efb42e3bb69fb4337
Will Fleming <wfleming77@gmail.com>
2021-02-02 18:22:32 -0500

    Move more config to use-package

--- Dotfiles/emacs.d/custom/01-common-setup.el
+++ Dotfiles/emacs.d/custom/01-common-setup.el
@@ -25,7 +25,7 @@
    (or (package-installed-p package)
        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
            (package-install package))))
- '(all-the-icons color-identifiers-mode csharp-mode doom-modeline elixir-mode exunit flycheck-inline git-gutter hydra js2-mode lua-mode projectile rainbow-delimiters rainbow-mode scala-mode shader-mode tern tide lsp-metals))
+ '(all-the-icons color-identifiers-mode doom-modeline elixir-mode exunit flycheck-inline hydra js2-mode projectile rainbow-delimiters rainbow-mode scala-mode tern tide lsp-metals))
 
 
 ;; Note that for all-the-icons to work you must manually install them by calling
