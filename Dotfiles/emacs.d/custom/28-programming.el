e7b47d5351af6539b29cdad6bef1f93dc6a7896e
Will Fleming <wfleming77@gmail.com>
2021-07-15 22:12:59 -0400

    enable fuzzy completion in cider-mode autocomplete

--- Dotfiles/emacs.d/custom/28-programming.el
+++ Dotfiles/emacs.d/custom/28-programming.el
@@ -94,6 +94,9 @@
 (use-package cider
   :ensure t)
 
+(add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
+
 ;; ---- Elixir ----
 ;; Install a language server
 ;; Download from https://github.com/elixir-lsp/elixir-ls/releases
