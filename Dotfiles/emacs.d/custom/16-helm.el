c5b85df7603ba3dccebdf93f416936f4500dc0ed
Will Fleming <wfleming77@gmail.com>
2022-12-17 12:35:46 -0500

    don't require helm-config

--- Dotfiles/emacs.d/custom/16-helm.el
+++ Dotfiles/emacs.d/custom/16-helm.el
@@ -7,7 +7,6 @@
          ("C-c C-h SPC" . helm-all-mark-rings))
   :config
   (progn
-    (require 'helm-config)
     (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
           helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
           helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
