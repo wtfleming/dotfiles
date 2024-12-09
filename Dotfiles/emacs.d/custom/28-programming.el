ab5c5131cddf68528a550758bc07d4f01daa920b
Will Fleming <wfleming77@gmail.com>
2024-12-09 11:27:40 -0500

    use tree-sitter for typescript in emacs

--- Dotfiles/emacs.d/custom/28-programming.el
+++ Dotfiles/emacs.d/custom/28-programming.el
@@ -68,6 +68,79 @@
 
 (setq flycheck-checker-error-threshold 1500)
 
+
+;; ----- tree-sitter -----
+;; TODO look at https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
+
+(use-package treesit
+      :mode (;;("\\.tsx\\'" . tsx-ts-mode)
+             ;;("\\.js\\'"  . typescript-ts-mode)
+             ;;("\\.mjs\\'" . typescript-ts-mode)
+             ;;("\\.mts\\'" . typescript-ts-mode)
+             ;;("\\.cjs\\'" . typescript-ts-mode)
+             ("\\.ts\\'"  . typescript-ts-mode)
+             ;;("\\.jsx\\'" . tsx-ts-mode)
+             ;;("\\.json\\'" .  json-ts-mode)
+             ;;("\\.Dockerfile\\'" . dockerfile-ts-mode)
+             ;;("\\.prisma\\'" . prisma-ts-mode)
+             ;; More modes defined here...
+             )
+      :preface
+      (defun os/setup-install-grammars ()
+        "Install Tree-sitter grammars if they are absent."
+        (interactive)
+        (dolist (grammar
+                 '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+                   (bash "https://github.com/tree-sitter/tree-sitter-bash")
+                   (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+                   (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+                   (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+                   (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+                   (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
+                   (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+                   (make "https://github.com/alemuller/tree-sitter-make")
+                   (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+                   (cmake "https://github.com/uyha/tree-sitter-cmake")
+                   (c "https://github.com/tree-sitter/tree-sitter-c")
+                   (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+                   (toml "https://github.com/tree-sitter/tree-sitter-toml")
+                   (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+                   (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+                   (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+                   (prisma "https://github.com/victorhqc/tree-sitter-prisma")))
+          (add-to-list 'treesit-language-source-alist grammar)
+          ;; Only install `grammar' if we don't already have it
+          ;; installed. However, if you want to *update* a grammar then
+          ;; this obviously prevents that from happening.
+          (unless (treesit-language-available-p (car grammar))
+            (treesit-install-language-grammar (car grammar)))))
+
+      ;; Optional, but recommended. Tree-sitter enabled major modes are
+      ;; distinct from their ordinary counterparts.
+      ;;
+      ;; You can remap major modes with `major-mode-remap-alist'. Note
+      ;; that this does *not* extend to hooks! Make sure you migrate them
+      ;; also
+      (dolist (mapping
+               '(;;(python-mode . python-ts-mode)
+                 ;;(css-mode . css-ts-mode)
+                 (typescript-mode . typescript-ts-mode)
+                 ;;(js-mode . typescript-ts-mode)
+                 ;;(js2-mode . typescript-ts-mode)
+                 ;;(c-mode . c-ts-mode)
+                 ;;(c++-mode . c++-ts-mode)
+                 ;;(c-or-c++-mode . c-or-c++-ts-mode)
+                 ;;(bash-mode . bash-ts-mode)
+                 ;;(css-mode . css-ts-mode)
+                 ;;(json-mode . json-ts-mode)
+                 ;;(js-json-mode . json-ts-mode)
+                 ;;(sh-mode . bash-ts-mode)
+                 ;;(sh-base-mode . bash-ts-mode)
+                 ))
+        (add-to-list 'major-mode-remap-alist mapping))
+      :config
+      (os/setup-install-grammars))
+
 ;; ----- Misc -----
 (use-package rainbow-delimiters
   :ensure t
@@ -101,6 +174,7 @@
 (add-hook 'rust-mode-hook (lambda () (subword-mode +1)))
 (add-hook 'yaml-mode-hook (lambda () (subword-mode +1)))
 (add-hook 'terraform-mode-hook (lambda () (subword-mode +1)))
+(add-hook 'typescript-mode-hook (lambda () (subword-mode +1)))
 
 ;; ----- git-gutter -----
 (use-package git-gutter
@@ -198,35 +272,45 @@
 
 ;; ------- TypeScript -------
 
-;; TypeScript Interactive Development Environment for Emacs
-;; https://github.com/ananthakumaran/tide
-(use-package tide
-  :ensure t)
+;; ADDED TODAY - can remove?
+;;(add-to-list 'auto-mode-alist '("\\.ts\\'" . js2-mode))
 
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  ; (flycheck-select-checker 'typescript-tslint)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
+;; See
+;; https://www.ovistoica.com/blog/2024-7-05-modern-emacs-typescript-web-tsx-config
+;; https://vxlabs.com/2022/06/12/typescript-development-with-emacs-tree-sitter-and-lsp-in-2022/
+
+
+;; ;; TypeScript Interactive Development Environment for Emacs
+;; ;; https://github.com/ananthakumaran/tide
+;; (use-package tide
+;;   :ensure t)
+
+;; (defun setup-tide-mode ()
+;;   (interactive)
+;;   (tide-setup)
+;;   (flycheck-mode +1)
+;;   ; (flycheck-select-checker 'typescript-tslint)
+;;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+;;   (eldoc-mode +1)
+;;   (tide-hl-identifier-mode +1)
+;;   ;; company is an optional dependency. You have to
+;;   ;; install it separately via package-install
+;;   ;; `M-x package-install [ret] company`
+;;   (company-mode +1))
+
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-;; formats the buffer before saving
-;; (add-hook 'before-save-hook 'tide-format-before-save)
+;; ;; formats the buffer before saving
+;; ;; (add-hook 'before-save-hook 'tide-format-before-save)
 
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
+;; (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; Move cursor by camelCase
-(add-hook 'typescript-mode-hook (lambda () (subword-mode +1)))
+;;(add-hook 'typescript-mode-hook (lambda () (subword-mode +1)))
 
+;; TODO this was for tide dev env and can be removed
 (setq typescript-indent-level 2)
 
 
