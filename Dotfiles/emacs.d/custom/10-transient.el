1b6a55ffe9a143f38517e1a377f0bea58b625ef3
Will Fleming <wfleming77@gmail.com>
2025-04-21 20:23:20 -0400

    transient to open a random wikipedia page

--- Dotfiles/emacs.d/custom/10-transient.el
+++ Dotfiles/emacs.d/custom/10-transient.el
@@ -1,13 +1,29 @@
 (use-package transient
   :ensure t)
 
-(transient-define-prefix  tutorial-transient ()
-  "Some Emacs magic"
-  :info-manual "Surf system-test transient"
+(transient-define-prefix wtf-links ()
+  "Common links"
   ["Not defined yet"
-    ("p" "print message"      tutorial-print-message)]
+    ("w" "Wikipedia random page" (lambda () (interactive) (browse-url "https://en.wikipedia.org/wiki/Special:Random")))]
   [("q" "Quit"           transient-quit-one)])
 
-(defun tutorial-print-message (&optional args)
-  (interactive)
-  (print "hello world"))
+
+
+;; (transient-define-prefix  tutorial-transient ()
+;;   "Some Emacs magic"
+;;   :info-manual "Surf system-test transient"
+;;   ["Not defined yet"
+;;     ("p" "print message"      tutorial-print-message)]
+;;   [("q" "Quit"           transient-quit-one)])
+
+;; (defun tutorial-print-message (&optional args)
+;;   (interactive)
+;;   (print "hello world"))
+
+
+;; (transient-define-prefix tsc-hello ()
+;;   "Prefix that is minimal and uses an anonymous command suffix."
+;;   [("s" "call suffix"
+;;     (lambda ()
+;;       (interactive)
+;;       (message "Called a suffix")))])
